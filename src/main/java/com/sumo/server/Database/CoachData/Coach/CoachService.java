b73fe979f626c05275dec84f0d35f7a574a97d00
Kuba <kuba.nowakowski@sabre.com>
2022-07-21 16:56:16 +0200

    working solution for trainer

--- src/main/java/com/sumo/server/Database/CoachData/Coach/CoachService.java
+++ src/main/java/com/sumo/server/Database/CoachData/Coach/CoachService.java
@@ -1,5 +1,6 @@
 package com.sumo.server.Database.CoachData.Coach;
 
+import com.sumo.server.Database.TeamData.Club.Club;
 import com.sumo.server.Database.userData.PersonalDetails.PersonalDetails;
 
 import java.util.List;
@@ -9,6 +10,10 @@ public interface CoachService {
     List<Coach> getAllCoaches();
 
     List<Coach> getCoachesByPersonalDetails(List<PersonalDetails> personalDetails);
+
+    Coach getCoachesByPersonalDetails(PersonalDetails personalDetails);
+
+    List<Club> getClubAdministeredByCoach(Coach coach);
 
     Coach save(Coach coach);
 }
