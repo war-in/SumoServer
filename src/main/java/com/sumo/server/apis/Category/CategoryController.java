fc4c90b373747bd680fd52c8a1f007598790cb71
Gajkes <gikes123@gmail.com>
2022-07-21 21:31:34 +0200

    Added category listing endpoint

---
+++ src/main/java/com/sumo/server/apis/Category/CategoryController.java
@@ -0,0 +1,24 @@
+package com.sumo.server.apis.Category;
+
+import com.sumo.server.Database.CompetitionData.Category.Category;
+import com.sumo.server.Database.CompetitionData.Category.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+public class CategoryController {
+    final CategoryService categoryService;
+
+    @GetMapping()
+    public ResponseEntity<List<Category>> getCategory() {
+        return ResponseEntity.ok().body(categoryService.getAll());
+    }
+}
